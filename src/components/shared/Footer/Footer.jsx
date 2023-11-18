ab40f545db330d66da144985c18f4848557e5cf0
mshipan <mshipan657@gmail.com>
2023-11-19 01:29:48 +0600

    update footer component

---
+++ src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,95 @@
+import Button from "../Button";
+import "./Footer.css";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { FaGithub } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.png";
+
+const Footer = () => {
+  return (
+    <div className="pt-20">
+      <div className="flex flex-col items-center gap-6 footerBg py-16">
+        <h1 className="text-[#55e6a5] text-2xl font-light font-poppins capitalize">
+          get latest updates
+        </h1>
+        <div className="flex flex-col items-center gap-5">
+          <h1 className="text-white text-5xl font-bold font-notoSans capitalize">
+            subscribe for newsletter
+          </h1>
+          <div className="w-full mt-5 border border-[#55e6a5]">
+            <form className="flex w-full">
+              <div className="form-control w-3/4">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter Your Email"
+                  className="py-3 pl-4 outline-none placeholder:text-white text-white"
+                />
+              </div>
+              <Button
+                text="subscribe now"
+                type="submit"
+                className="flex items-center gap-1 capitalize w-1/4"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-8 my-16">
+        <div className="flex items-center gap-4">
+          <div className="p-5 border border-zinc-800 hover:border-white rounded-full text-white hover:text-[#02050a] hover:bg-white transition-all ease-in-out duration-500">
+            <FaPhoneAlt />
+          </div>
+          <h1 className="font-poppins text-2xl text-white">+88 01622 543390</h1>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="p-5 border border-zinc-800 hover:border-white rounded-full text-white hover:text-[#02050a] hover:bg-white transition-all ease-in-out duration-500">
+            <IoIosMail />
+          </div>
+          <h1 className="font-poppins text-2xl text-white">
+            shipanmallik95@gmail.com
+          </h1>
+        </div>
+        <div className="flex items-center gap-4">
+          <Link to="https://github.com/mshipan">
+            <div className="p-5 border border-zinc-800 hover:border-white rounded-full text-white hover:text-[#02050a] hover:bg-white transition-all ease-in-out duration-500">
+              <FaGithub />
+            </div>
+          </Link>
+          <Link to="https://www.facebook.com/mallik.shipan">
+            <div className="p-5 border border-zinc-800 hover:border-white rounded-full text-white hover:text-[#02050a] hover:bg-white transition-all ease-in-out duration-500">
+              <FaFacebookF />
+            </div>
+          </Link>
+          <Link to="https://www.linkedin.com/in/shipan-mallik/">
+            <div className="p-5 border border-zinc-800 hover:border-white rounded-full text-white hover:text-[#02050a] hover:bg-white transition-all ease-in-out duration-500">
+              <FaLinkedin />
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div className="border-t border-zinc-800">
+        <div className="py-2">
+          <div className="md:w-3/5 w-full mx-auto flex items-center justify-between">
+            <p className="text-sm text-zinc-500 font-poppins">
+              &copy; 2023 All Right Reserved by
+              <Link to="/">
+                <span className="text-white"> Shipan Mallik</span>
+              </Link>
+            </p>
+            <div>
+              <Link to="/">
+                <img src={logo} alt="website logo" className="w-28" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,17 +1,57 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { useInView } from "react-intersection-observer";
 import Navbar from "../components/shared/Navbar/Navbar";
-import Footer from "../components/shared/Footer";
 import FollowMe from "../components/FollowMe";
 import EmailAndPhone from "../components/EmailAndPhone";
+import Footer from "../components/shared/Footer/Footer";
+import { useEffect } from "react";
 
 const MainLayouts = () => {
+  const [hideComponents, setHideComponents] = useState(false);
+
+  const [footerRef, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-50px 0px", // Adjust the root margin as needed
+  });
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (currentScrollPos > prevScrollPos) {
+        // Scrolling down
+        setHideComponents(inView);
+      } else {
+        // Scrolling up
+        setHideComponents(false);
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos, inView]);
+
   return (
     <div>
       <Navbar />
-      <EmailAndPhone />
-      <FollowMe />
+      {!hideComponents && (
+        <>
+          <EmailAndPhone />
+          <FollowMe />
+        </>
+      )}
       <Outlet />
-      <Footer />
+      <div ref={footerRef}>
+        <Footer />
+      </div>
     </div>
   );
 };
