586f18b435f1a0bd2c0a76c8c707bbbc98195915
Shipan Mallik <mshipan657@gmail.com>
2024-01-11 20:37:10 +0600

    create skills page in dashboard

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
 
-const ProgressBar = ({ text, value, isVisible, percentValue }) => {
+const ProgressBar = ({ text, value, isVisible, percentValue, className }) => {
   return (
-    <div className="bg-zinc-800 ">
+    <div className={`bg-zinc-800 ${className}`}>
       <div className="pt-3 pb-5 pl-5 pr-10 flex items-center justify-between">
         <h1 className="font-poppins font-medium text-white uppercase">
           {text}
