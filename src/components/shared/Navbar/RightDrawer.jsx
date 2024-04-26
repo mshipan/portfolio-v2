2d569355adb79ae160e9f33dbbbf982a3c3a7c3b
mshipan <mshipan657@gmail.com>
2024-04-26 13:54:48 +0600

    use the contact send main functionality to right drawer

--- src/components/shared/Navbar/RightDrawer.jsx
+++ src/components/shared/Navbar/RightDrawer.jsx
@@ -1,8 +1,44 @@
 import { Link } from "react-router-dom";
 import logo from "../../../assets/favicon.jpg";
 import Button from "../Button";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useSendEmailMutation } from "../../../redux/features/api/contact/contactApi";
+import toast from "react-hot-toast";
+import checkGif from "../../../assets/check.gif";
 
 const RightDrawer = () => {
+  const [loading, setLoading] = useState(false);
+  const [sentText, setSentText] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const [sendEmail] = useSendEmailMutation();
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      const result = await sendEmail(data);
+
+      // Show toast based on response
+      if (result.data) {
+        reset();
+        setLoading(false);
+        toast.success("Message sent successfully.");
+        setSentText(true);
+        setTimeout(() => {
+          setSentText(false);
+        }, 1500);
+      } else {
+        toast.error("Failed to send message.");
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error(`An error occurred: ${error}`);
+    }
+  };
   return (
     <div>
       <div className="bg-[#141c27]">
@@ -30,36 +66,106 @@ const RightDrawer = () => {
           Get in touch
         </h1>
         <div>
-          <form className="flex flex-col gap-5">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-5"
+          >
             <div className="form-control">
+              {errors.name && (
+                <p className="text-yellow-500 text-xs font-notoSans mb-2">
+                  Name is Required **
+                </p>
+              )}
+
               <input
                 type="text"
+                name="name"
+                {...register("name", { required: true })}
                 placeholder="Your Name"
-                className="border border-[#55e6a5] p-3 bg-[#141c27] placeholder-zinc-300 outline-none"
+                className="border border-[#55e6a5] p-3 bg-[#141c27] placeholder-zinc-300 outline-none text-white"
               />
             </div>
             <div className="form-control">
+              {errors.email && (
+                <p className="text-yellow-500 text-xs font-notoSans mb-2">
+                  Email is Required **
+                </p>
+              )}
+
               <input
                 type="email"
+                name="email"
+                {...register("email", { required: true })}
                 placeholder="Your Email"
-                className="border border-[#55e6a5] p-3 bg-[#141c27] placeholder-zinc-300 outline-none"
+                className="border border-[#55e6a5] p-3 bg-[#141c27] placeholder-zinc-300 outline-none text-white"
               />
             </div>
             <div className="form-control">
+              {errors.phone && (
+                <p className="text-yellow-500 text-xs font-notoSans mb-2">
+                  Phone is Required **
+                </p>
+              )}
+
+              <input
+                type="text"
+                name="phone"
+                {...register("phone", { required: true })}
+                placeholder="Your Phone"
+                className="border border-[#55e6a5] p-3 bg-[#141c27] placeholder-zinc-300 outline-none text-white"
+              />
+            </div>
+            <div className="form-control">
+              {errors.subject && (
+                <p className="text-yellow-500 text-xs font-notoSans mb-2">
+                  Subject is Required **
+                </p>
+              )}
+
+              <input
+                type="text"
+                name="subject"
+                {...register("subject", { required: true })}
+                placeholder="Subject"
+                className="border border-[#55e6a5] p-3 bg-[#141c27] placeholder-zinc-300 outline-none text-white"
+              />
+            </div>
+            <div className="form-control">
+              {errors.comment && (
+                <p className="text-yellow-500 text-xs font-notoSans mb-2">
+                  Comment is Required **
+                </p>
+              )}
+
               <textarea
-                name=""
+                name="comment"
+                {...register("comment", { required: true })}
                 cols="20"
                 rows="5"
-                placeholder="Message"
-                className="border border-[#55e6a5] p-3 bg-[#141c27] placeholder-zinc-300 outline-none"
+                placeholder="Message..."
+                className="border border-[#55e6a5] p-3 bg-[#141c27] placeholder-zinc-300 outline-none text-white"
               ></textarea>
             </div>
             <div>
               <Button
-                text="Submit Now"
+                text={
+                  loading ? (
+                    <div className="flex items-center justify-center gap-1">
+                      <span className="loading loading-spinner loading-sm"></span>
+                      <span>Sending...</span>
+                    </div>
+                  ) : sentText ? (
+                    <div className="flex items-center justify-center gap-1">
+                      <img src={checkGif} alt="Sent GIF" className="w-5 h-5" />
+                      <span>Mail sent.</span>
+                    </div>
+                  ) : (
+                    "Send Mail"
+                  )
+                }
                 type="submit"
-                className="lowercase"
-              ></Button>
+                className="flex items-center justify-center gap-1 capitalize w-full"
+              />
             </div>
           </form>
         </div>
